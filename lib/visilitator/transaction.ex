11a0b5f2d5a460271f027ddd3932ca0b083ec191
Max Wilson <max@scriptdrop.co>
2022-11-12 17:40:01 -0500

    move transactions into postgres; multi transaction

--- lib/visilitator/transaction.ex
+++ lib/visilitator/transaction.ex
@@ -2,35 +2,50 @@ defmodule Visilitator.Transaction do
   @moduledoc """
   Transaction object
   """
+  use Ecto.Schema
 
   alias Visilitator.Repo
   alias Visilitator.User
   alias Visilitator.Visit
 
-  @type t :: %__MODULE__{
-          id: String.t(),
-          member_id: integer(),
-          pal_id: integer(),
-          visit_id: String.t()
-        }
-  defstruct [:id, :member_id, :pal_id, :visit_id]
+  @type t :: %__MODULE__{}
+
+  schema "transactions" do
+    field(:member, :integer)
+    field(:pal, :integer)
+    field(:visit, :integer)
+  end
 
   @doc """
   Given member_id, pal_id, and visit_id, this function creates, persists to storage, and returns a Transaction
   """
   @spec create(User.t(), Visit.t()) :: {t(), User.t(), User.t()}
   def create(pal = %User{}, visit = %Visit{}) do
-    # Good place for an ecto multi
-    User
-    |> Repo.get(visit.member)
-    |> User.debit(visit)
+    member = User |> Repo.get(visit.member)
+    debited_amount = member |> User.total_after_debit(visit)
+    fulfilled_amount = pal |> User.total_after_fulfillment(visit)
 
-    pal
-    |> User.fulfill(visit)
+    transaction = %__MODULE__{
+      member: visit.member,
+      pal: pal.id,
+      visit: visit.id
+    }
 
-    tid = UUID.uuid4()
-    txn = %__MODULE__{id: tid, member_id: visit.member, pal_id: pal.id, visit_id: visit.id}
-    :ets.insert(:transactions, {tid, txn})
-    {txn, User |> Repo.get(visit.member), User |> Repo.get(pal.id)}
+    {:ok, changes} =
+      Ecto.Multi.new()
+      |> Ecto.Multi.update(:debit_member, User.debit(member, visit))
+      |> Ecto.Multi.run(:check_debit, fn
+        _repo, %{debit_member: %User{balance: ^debited_amount}} -> {:ok, nil}
+        _repo, %{debit_member: _} -> {:error, {:failed_debit, member}}
+      end)
+      |> Ecto.Multi.update(:fulfill_pal, User.fulfill(pal, visit))
+      |> Ecto.Multi.run(:check_fulfill, fn
+        _repo, %{fulfill_pal: %User{balance: ^fulfilled_amount}} -> {:ok, nil}
+        _repo, %{fulfill_pal: _} -> {:error, {:failed_fulfill, pal}}
+      end)
+      |> Ecto.Multi.insert(:create_transaction, transaction)
+      |> Repo.transaction()
+
+    {changes.create_transaction, changes.debit_member, changes.fulfill_pal}
   end
 end
